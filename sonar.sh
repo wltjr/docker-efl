d769e628803d5d33c5f20974e8c38219c243550a
William L. Thomson Jr. <w@wltjr.com>
2026-01-28 14:13:05 -0500

    sonar: Unzip to unversioned directory, pita to drop version...

--- sonar.sh
+++ sonar.sh
@@ -16,9 +16,10 @@ mv -v lib*.so /usr/bin
 
 # download and unpack sonar-scanner
 sv=8.0.1.6346
-curl -k -o sonar-scanner-cli-linux-x64.zip \
+curl -k -L -o sonar-scanner-cli-linux-x64.zip \
 	-L https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${sv}-linux-x64.zip
-unzip -q sonar-scanner-cli-linux-x64.zip
+unzip -q sonar-scanner-cli-linux-x64.zip \
+	-d sonar-scanner-linux-x64
 echo "export PATH=/sonar-scanner-linux-x64/bin:${PATH}" >> /root/.bashrc
 
 rm *.zip

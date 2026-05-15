908f6a3683591a3744512a69ed8706649b92a33c
William L. Thomson Jr. <w@wltjr.com>
2026-05-15 10:44:18 -0400

    sonar: Bump build wrapper 6.79.1.98186 -> 6.81.1.99296

--- sonar.sh
+++ sonar.sh
@@ -5,7 +5,7 @@
 
 # edit commented version and commit to update the build-wrapper version
 #
-# use analyzer/build-wrapper version 6.79.1.98186
+# use analyzer/build-wrapper version 6.81.1.99296
 #
 # download, unpack and relocate build-wrapper-linux-x86-64
 curl -JLO -k https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
